8716a12a55e3169f8f749656b894bd1fb22eef53
JonathanFranco21 <twojfox@outlook.com>
2023-08-06 20:25:09 -0700

    Version 1.0.1

--- functions.js
+++ functions.js
@@ -1,17 +1,21 @@
 var countDownDate1 = new Date("Aug 17, 2023 00:00:00").getTime();
-var countDownDate2 = new Date("Oct 6, 2023 00:00:00").getTime();
+var countDownDate2 = new Date("Sep 6, 2023 00:00:00").getTime();
+var countDownDate3 = new Date("Oct 6, 2023 00:00:00").getTime();
 //var countDownDate3 = new Date("Oct 13, 2023 00:00:00").getTime();
 //var countDownDate3 = new Date("Oct 20, 2023 00:00:00").getTime();
 //var countDownDate3 = new Date("Oct 27, 2023 00:00:00").getTime();
 //var countDownDate3 = new Date("Nov 3, 2023 00:00:00").getTime();
-var countDownDate3 = new Date("Nov 10, 2023 00:00:00").getTime();
+var countDownDate4 = new Date("Nov 10, 2023 00:00:00").getTime();
 //var countDownDate4 = new Date("Nov 10, 2023 00:00:00").getTime();
 //var countDownDate4 = new Date("Nov 10, 2023 00:00:00").getTime();
-//var countDownDate4 = new Date("Nov 29, 2023 00:00:00").getTime();
+var countDownDate5 = new Date("Nov 29, 2023 00:00:00").getTime();
 
 var timer1 = document.getElementById("timer1");
 var timer2 = document.getElementById("timer2");
 var timer3 = document.getElementById("timer3");
+var timer4 = document.getElementById("timer4");
+var timer5 = document.getElementById("timer5");
+
 
 function countdown(finish_date, timer){
   var x = setInterval(function() {
