0db0f9b2695b3842c06ab76abbdd2f5ce5d9d837
JonathanFranco21 <twojfox@outlook.com>
2023-11-29 17:18:46 -0800

    Version 1.0.2

---
+++ hider.js
@@ -0,0 +1,75 @@
+function contentHider1() {
+  var x = document.getElementById("violenceAndDisturbingContentArea");
+  
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function contentHider2() {
+  var x = document.getElementById("suggestiveContentArea");
+
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function contentHider3() {
+  var x = document.getElementById("drinkingDrugAndGamblingContentArea");
+
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function contentHider4() {
+  var x = document.getElementById("languageArea");
+
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function yearRevealAndHide5() {
+  var y = document.getElementById("discography5");
+  if (y.style.display == "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
+
+function yearRevealAndHide6() {
+  var y = document.getElementById("discography6");
+  if (y.style.display == "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
+
+function yearRevealAndHide7() {
+  var y = document.getElementById("discography7");
+  if (y.style.display == "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
+
+function yearRevealAndHide8() {
+  var y = document.getElementById("discography8");
+  if (y.style.display == "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
+}
